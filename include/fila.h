4d33e81eb26b457ecd8758a8fbbe29fea569554d
Davi Alves <davialvesr18@gmail.com>
2025-02-27 13:37:57 -0300

    .

--- include/fila.h
+++ include/fila.h
@@ -8,19 +8,25 @@ using std::cout;
 using std::endl;
 
 class fila {
-    int tam;
-    int head;
-    int end;
+    int tam; // Guarda o tamanho atual
+    int head; // Guarda a posição do primeiro elemento
+    int end; // Guarda a posição do último elemento
     int vetor[TAM_MAX];
 public:
     fila();
 
     bool empty();
+
     bool full();
+    
     int get_tam();
+    
     void insert(int v);
+    
     void remove();
+        
     int get_head();
+
     void print();
 
 };
